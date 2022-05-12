a446a31f545e582ab6ab69df759c99bb4d3aec0b
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-05-13 00:39:48 +0100

    Added Hamburger Menu

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const token = "coinranking64cde228d1852cd27131b0dba9371a17bc09d58764fbe1ae";
+const mobileMenu = document.querySelector(".mobile-menu");
+const nav =  document.querySelector("nav");
+const links = document.querySelectorAll("nav ul li");
 
 class CryptoExchange{
+
   constructor(){
+    mobileMenu.addEventListener("click", this.mobileMenu);
+    this.mobileMenuLinks();
     this.currencies();
+  }
+
+  // Show and hide hamburger menu
+  mobileMenu(){
+    mobileMenu.classList.toggle("active");
+    nav.classList.toggle("active");
+  }
+
+  // Hide hamburger menu as the user clicks a link
+  mobileMenuLinks(){
+    links.forEach(link => {
+      link.addEventListener("click", () => {
+        mobileMenu.classList.toggle("active");
+        nav.classList.toggle("active");
+      });
+    });
   }
 
   async currencies(){
