f33c747bd3c251dac228a2f1e8b60960a40867b5
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-05-19 08:42:46 +0100

    Changed CORS policy to 'no-cors'

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ class CryptoExchange{
   static async currencies(){
     const currencies = await fetch("https://api.coinranking.com/v2/coins", {
       // method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors' , // no-cors, *cors, same-origin
+      mode: 'no-cors' , // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials:'same-origin' , // include, *same-origin, omit
       headers: {
