ab5526cae206f3807af2c313653f7c0cf38595ed
Cyber9ja <ayodejioladapo96@gmail.com>
2022-08-08 12:26:37 +0100

    Updated read me

---
+++ README.md
@@ -0,0 +1,3 @@
+Latest crypto market, historical data and news
+
+https://coinlib.io/ https://www.coinlore.com/cryptocurrency-data-api https://www.cryptingup.com/apidoc/#api-architecture https://newsapi.org/docs/get-started#search https://www.cryptingup.com/apidoc/#api-architecture https://messari.io/api/docs#operation/Get%20all%20Markets https://www.thenewsapi.com
--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -7,10 +7,6 @@ import { formatPrice } from '../utilities/formatNumber';
 
 
 const Crypto = ({ coin, allCoins, price, marketCap, change, symbol }) => {
-  // let backgroundColor = formatChange(coin.change);
-  // console.log(coin.change);
-  // if(coin !== undefined) console.log(formatChange(coin.change));
-
   return (
     <div className='coins-row'>
       <div className="all-coins">
@@ -32,12 +28,17 @@ const Crypto = ({ coin, allCoins, price, marketCap, change, symbol }) => {
         <p className='market-cap'>{marketCap === undefined ? (millify(coin?.marketCap)) : 'Market Cap'}</p>
       </div>
       <div className="change-wrapper">
-        {/* <p className='change' style={coin && {backgroundColor: `${formatChange(coin.change)}`}}>{change === undefined ? `${coin?.change}%` : '24h'}</p> */}
-        {/* <p className='change' style={change === undefined ? coin?.change > 0 ? {backgroundColor: 'green'} : {backgroundColor: 'red'}  : {backgroundColor: 'white'}}>{change === undefined ? `${coin?.change}%` : '24h'}</p> */}
-        {/* <p className='change' style={change === undefined ? { backgroundColor: coin?.change > 0 ? 'green' : 'red'}  : {backgroundColor: 'white'}}>{change === undefined ? `${coin?.change}%` : '24h'}</p> */}
-        <p className='change' style={{backgroundColor: change === undefined ? coin?.change >= 0 ? 'green': 'red' : 'inherit'}}>{change === undefined ? `${coin?.change}%` : '24h'}</p>
+        <p 
+          className='change' 
+          style={
+            {
+              backgroundColor: change === undefined ? coin?.change >= 0 ? 'green': 'red' : 'inherit', 
+              color: change===undefined ? 'white' : 'inherit'
+            }
+          }
+            >{change === undefined ? `${coin?.change}%` : '24h'}
+        </p>
 
-        {/* {backgroundColor: `${formatChange(coin.change)}`} */}
       </div>
     </div>
   )
