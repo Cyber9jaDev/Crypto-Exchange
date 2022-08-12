c04b0e5decb2ce97e0b125fc30eb144f8c19c957
Cyber9ja <ayodejioladapo96@gmail.com>
2022-08-12 17:21:53 +0100

    Added scss and modified select button

--- src/components/selectChartPeriod.js
+++ src/components/selectChartPeriod.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './styles/chart.scss';
+
 
 const SelectChartPeriod = ({ chartPeriod, setChartPeriod }) => {
   
@@ -7,18 +9,20 @@ const SelectChartPeriod = ({ chartPeriod, setChartPeriod }) => {
   }
 
   return (
-    <select onChange={changeChartPeriod} value= {chartPeriod} name="chartPeriod" >
-      <option value="1h">1h</option>
-      <option value="3h">3h</option>
-      <option value="12h">12h</option>
-      <option value="24h">24h</option>
-      <option value="7d">7d</option>
-      <option value="30d">30d</option>
-      <option value="3m">3m</option>
-      <option value="1y">1y</option>
-      <option value="3y">3y</option>
-      <option value="5y">5y</option>
-    </select>
+    <div className="chart-wrapper">
+      <select onChange={changeChartPeriod} value= {chartPeriod} name="chartPeriod" >
+        <option value="1h">Last 1hour</option>
+        <option value="3h">Last 3hours</option>
+        <option value="12h">Last 12hours</option>
+        <option value="24h">Last 24hours</option>
+        <option value="7d">Last 7days</option>
+        <option value="30d">Last 30days</option>
+        <option value="3m">Last 3months</option>
+        <option value="1y">Last 1year</option>
+        <option value="3y">Last 3years</option>
+        <option value="5y">Last 5years</option>
+      </select>
+    </div>
   )
 }
 
