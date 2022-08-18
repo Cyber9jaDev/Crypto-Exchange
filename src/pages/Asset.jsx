aefcf4043ef99da62379c71f4698cfe2b74b45f4
Cyber9ja <ayodejioladapo96@gmail.com>
2022-08-18 18:35:20 +0100

    sorted

--- src/pages/Asset.jsx
+++ src/pages/Asset.jsx
@@ -13,13 +13,14 @@ import { AssetError, SingleAssetChart, ROI, Summary, KeyMetrics, HistoricalData,
 const Asset = () => {
   const [active, setActive] = useState('overview');
   const { asset_symbol } = useParams();
-  const { chartPeriod, setChartPeriod } = useContext(ChartPeriodContext);
+  const { chartPeriod, setChartPeriod, setChartPeriod } = useContext(ChartPeriodContext);
   const { data : assetInformation } = useApi(`coin/${uuids[asset_symbol]}?timePeriod=${chartPeriod}`, process.env.REACT_APP_COINRANKING_URL, useHeaders().coinrankingHeader);
   const { data : profile } = useApi(`/v2/assets/${asset_symbol}/profile`, process.env.REACT_APP_MESSARI_URL, useHeaders().messariHeader);
   const { data : metrics } = useApi(`/v1/assets/${asset_symbol}/metrics`, process.env.REACT_APP_MESSARI_URL, useHeaders().messariHeader);
 
   const handleClick = (e) => setActive(() => e.target.name);
 
+  // console.log(metrics);
   // console.log(metrics);
   // console.log(profile);
   // console.log(assetInformation)
@@ -48,16 +49,16 @@ const Asset = () => {
               <FontAwesomeIcon className='share-icon' icon={faShareNodes} />
             </div>
 
-            <div className="profile-links">
-              <p className='overview'><Link to='' className={ active === 'overview' ? 'link active' : 'link' } name='overview' onClick={handleClick}>Overview</Link></p>
-              <p className='market'><Link to='' className={ active === 'market' ? 'link active' : 'link' } name='market' onClick={handleClick}>Market</Link></p>
-              <p className='metrics'><Link to='' className={ active === 'metrics' ? 'link active' : 'link' } name='metrics' onClick={handleClick}>Metrics</Link></p>
-              <p className='historical-data'><Link to='' className={ active === 'historical-data' ? 'link active' : 'link' } name='historical-data' onClick={handleClick}>Historical Data</Link></p>
-              <p className='charts'><Link to='' className={ active === 'charts' ? 'link active' : 'link' } name='charts' onClick={handleClick}>Charts</Link></p>
-              <p className='research'><Link to='' className={ active === 'research' ? 'link active' : 'link' } name='research' onClick={handleClick}>Research</Link></p>
-              <p className='news'><Link to='' className={ active === 'news' ? 'link active' : 'link' } name='news' onClick={handleClick}>News</Link></p>
-              <p className='events'><Link to='' className={ active === 'events' ? 'link active' : 'link' } name='events' onClick={handleClick}>Events</Link></p>
-            </div>
+                <div className="profile-links">
+                  <p className='overview'><Link to='' className={ active === 'overview' ? 'link active' : 'link' } name='overview' onClick={handleClick}>Overview</Link></p>
+                  <p className='market'><Link to='' className={ active === 'market' ? 'link active' : 'link' } name='market' onClick={handleClick}>Market</Link></p>
+                  <p className='metrics'><Link to='' className={ active === 'metrics' ? 'link active' : 'link' } name='metrics' onClick={handleClick}>Metrics</Link></p>
+                  <p className='historical-data'><Link to='' className={ active === 'historical-data' ? 'link active' : 'link' } name='historical-data' onClick={handleClick}>Historical Data</Link></p>
+                  <p className='charts'><Link to='' className={ active === 'charts' ? 'link active' : 'link' } name='charts' onClick={handleClick}>Charts</Link></p>
+                  <p className='research'><Link to='' className={ active === 'research' ? 'link active' : 'link' } name='research' onClick={handleClick}>Research</Link></p>
+                  <p className='news'><Link to='' className={ active === 'news' ? 'link active' : 'link' } name='news' onClick={handleClick}>News</Link></p>
+                  <p className='events'><Link to='' className={ active === 'events' ? 'link active' : 'link' } name='events' onClick={handleClick}>Events</Link></p>
+                </div>
 
             <div className="overview">
               <div className="col-1">
